2484d94cb47bcfdd55a5fcfaef476a405c123e15
Jesus <robinson.tech.digital@gmail.com>
2021-07-05 20:13:06 -0500

    Initial commit.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+#This code can save power computation by caching the inverse of a matrix.
 
-## Write a short comment describing this function
+#makeCacheMatrix allows to enter a new matrix into memory. It is assumed that
+#the input is a square matrix and it's not singular.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL }
+  get <- function() x
+  setinv <- function(solve) i <<- solve
+  getinv <- function() i
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+#cacheSolve searches into cache for inverse of the previous matrix. If not,
+#it calculates the inverse by using the 'solve' function. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)){
+    message("getting cached data")
+    return(i) }
+  mat <- x$get()
+  i <- solve(mat)
+  x$setinv(i)
+  i
 }
